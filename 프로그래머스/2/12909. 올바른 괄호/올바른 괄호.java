8e59501040975953a6a11ba370b2a9823c9d4a2f
jeendale <128301772+jeendale@users.noreply.github.com>
2024-03-21 02:16:48 +0900

    [level 2] Title: 올바른 괄호, Time: 14.41 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,13 +4,15 @@ class Solution {
     boolean solution(String s) {
         ArrayDeque<Character> stack=new ArrayDeque<>();
         char[] arr=s.toCharArray();
-        
+        int cnt=0;
         for(char a :arr){
+            cnt++;
             if(a=='('){
                 stack.push(a);
             }
             else{
                 if(stack.isEmpty()||stack.pop()==a){
+                    System.out.println(a+" "+cnt);
                     return false;
                 }
             }
