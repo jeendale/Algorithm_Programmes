5debcca14b44deb135b2ce8078fd26ba7a8af436
jeendale <128301772+jeendale@users.noreply.github.com>
2024-05-08 23:32:51 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,23 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
+        int answer=0;
         HashMap<String,Integer> list =new HashMap<>();
+        
         for(int i=0;i<want.length;i++){
             list.put(want[i],number[i]);
         }
-        int answer=0;
-        for(int i=0;i<discount.length-9;i++){
-            HashMap<String,Integer> checkList=new HashMap<>();
-            
-            for(int j=i;j<i+10;j++){
-                if(list.containsKey(discount[j])){
-                    checkList.put(discount[j],checkList.getOrDefault(discount[j],0)+1);
-                }
-            }
-            if(list.equals(checkList))
-                answer++;
-        }
         
+        for(int i=0;i<discount.length-9;i++){
+            if(!list.containsKey(discount[i])){
+                continue;
+            }
+                
+            HashMap<String,Integer> tenDayList=new HashMap<>();
+            for(int j=i;j<i+10;j++){
+                tenDayList.put(discount[j],tenDayList.getOrDefault(discount[j],0)+1);
+            }
+            if(tenDayList.equals(list)){
+                answer++;
+            }
+        }
         return answer;
     }
 }
