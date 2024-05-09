08e5d931bc59df042050872c110fea9d03af6644
jeendale <128301772+jeendale@users.noreply.github.com>
2024-05-09 23:25:29 +0900

    [level 2] Title: 오픈채팅방, Time: 110.20 ms, Memory: 156 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,24 +1,25 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(String[] record) {
-        HashMap<String,String> chat=new HashMap<>();
-        chat.put("Enter","님이 들어왔습니다.");
-        chat.put("Leave","님이 나갔습니다.");
+        HashMap<String,String> msg= new HashMap<>();
+        msg.put("Enter","님이 들어왔습니다.");
+        msg.put("Leave","님이 나갔습니다.");
         
-        HashMap<String,String> user=new HashMap<>();
+        HashMap<String,String> user =new HashMap<>();
         for(String str : record){
-            String[] cmd=str.split(" ");
+            String[] cmd =str.split(" ");
             if(cmd.length==3){
                 user.put(cmd[1],cmd[2]);
             }
         }
-        ArrayList<String> answer =new ArrayList<>();
-        for(String str: record){
+        ArrayList<String> result=new ArrayList<>();
+        for(String str:record){
             String[] cmd=str.split(" ");
-            if(chat.containsKey(cmd[0])){
-                answer.add(user.get(cmd[1])+chat.get(cmd[0]));
+            if(msg.containsKey(cmd[0])){
+                result.add(user.get(cmd[1])+msg.get(cmd[0]));
             }
         }
-        return answer.toArray(new String[0]);
+        return result.toArray(new String[0]);
     }
 }
